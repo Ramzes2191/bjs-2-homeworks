49d968a53842b6c30f1724d7d2edcac058927669
Rudolf <ramazanov.rudolf@yandex.ru>
2026-04-07 19:57:20 +0500

    Реализована задача 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let D
+  let X1
+  let X2
+	D = b ** 2 - 4 * a * c
+	if (D < 0) {
+		arr = []
+		console.log("Корней нет")
+	} else if (D === 0) {
+		X1 = -b / (2 * a)
+		arr.push(X1)
+    console.log("Один корень")
+	} else if (D > 0) {
+		X1 = (-b + Math.sqrt(D)) / (2 * a)
+		X2 = (-b - Math.sqrt(D)) / (2 * a)
+		arr.push(X1, X2)
+    console.log("Два корня")
+	}
+
+	return arr;
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
